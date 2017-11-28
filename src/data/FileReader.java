388c4ebc46648e6e37a66da10bfce5c64baf1f76
Archie <Archiez0077@gmail.com>
2017-11-28 09:59:40 -0600

    Added to FileReader

--- src/data/FileReader.java
+++ src/data/FileReader.java
@@ -1,9 +1,11 @@
 package data;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FileReader {
 
@@ -17,5 +19,13 @@ public class FileReader {
 		PrintWriter printer = new PrintWriter(new FileOutputStream(mainFile));
 		printer.println(time + "    " + name + "    " + dose);
 		printer.close();
+	}
+	
+	public void sort() throws FileNotFoundException {
+		Scanner reader = new Scanner(mainFile);
+	}
+	
+	public void deleteFromFile() {
+		
 	}
 }
