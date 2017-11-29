4be8e3122de5bf2af9bb1b1b936e1f9540760b5a
Archie <Archiez0077@gmail.com>
2017-11-29 10:01:03 -0600

    Worked on filereader

--- src/data/FileReader.java
+++ src/data/FileReader.java
@@ -5,27 +5,48 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileReader {
 
 	private File mainFile;
+	private String path;
 
 	public FileReader(String path) {
 		mainFile = new File(path);
+		this.path = path;
 	}
 
 	public void saveToFile(int time, String name, int dose) throws IOException {
 		PrintWriter printer = new PrintWriter(new FileOutputStream(mainFile));
-		printer.println(time + "    " + name + "    " + dose);
+		printer.println(time + ":" + name + ":" + dose);
 		printer.close();
 	}
-	
+
 	public void sort() throws FileNotFoundException {
 		Scanner reader = new Scanner(mainFile);
+		String x = "";
+		while (reader.hasNext()) {
+			x = x + reader.next();
+		}
 	}
-	
-	public void deleteFromFile() {
-		
+
+	public void deleteFromFile(String del) throws FileNotFoundException {
+		Scanner reader = new Scanner(mainFile);
+		List<String> fileLines = new ArrayList<String>();
+		String x;
+		while(reader.hasNextLine()) {
+			x = reader.nextLine();
+			if(!x.equals(del));
+			fileLines.add(x);
+		}
+		mainFile.delete();
+		mainFile = new File(path);
+		PrintWriter printer = new PrintWriter(new FileOutputStream(mainFile));
+		while(fileLines.size()>0) {
+			fileLines
+		}
 	}
 }
