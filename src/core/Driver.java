dfe55eaf581a2c6468e985cbcdcc7cbe61fc6f74
Archie <Archiez0077@gmail.com>
2017-12-03 17:55:24 -0600

    Started on the GUI

--- src/core/Driver.java
+++ src/core/Driver.java
@@ -1,18 +1,34 @@
 package core;
 
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Driver extends Application {
 
-	protected Stage window;
-	public boolean running;
-
-	public void start(Stage x) {
-		window = x;
-		window.setTitle("Medicine Tracker");
-		window.set
-		running = true;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+	public void start(Stage mainStage) {
+		mainStage.setTitle("Medicine Tracker");
+		Button enter = new Button();
+		enter.setText("Start");
+		enter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Start!");
+			}
+		});
+		Pane root = new Pane();
+		root.getChildren().add(enter);
+		mainStage.setScene(new Scene(root, 300, 250));
+		mainStage.show();
 	}
 
 }
