7b3184100f1b37a4f196742d47571d5a25ab02a6
Archie <Archiez0077@gmail.com>
2017-12-04 18:57:16 -0600

    Added to the GUI

--- src/core/Driver.java
+++ src/core/Driver.java
@@ -1,15 +1,16 @@
 package core;
 
 
+import display.GUI;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-
-import display.GUI;
 
 public class Driver extends Application {
 
@@ -20,17 +21,21 @@ public class Driver extends Application {
 
 	public void start(Stage mainStage) {
 		mainStage.setTitle("Medicine Tracker");
+		mainStage.setResizable(false);
 		Button enter = new Button();
 		enter.setText("Start");
+		enter.setLayoutX(240);
+		enter.setLayoutY(165);
 		enter.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
-				GUI.pageOne(mainStage);
+				GUI.home(mainStage);
 			}
 		});
 		Pane root = new Pane();
+		root.getChildren().add(new Rectangle(500, 350, Color.ORANGERED));
 		root.getChildren().add(enter);
-		mainStage.setScene(new Scene(root, 300, 250));
+		mainStage.setScene(new Scene(root, 500, 350));
 		mainStage.show();
 	}
 
--- src/display/GUI.java
+++ src/display/GUI.java
@@ -5,28 +5,46 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GUI {
 
-	public static void pageOne(Stage xStage) {
+	public static void home(Stage xStage) {
 		xStage.setTitle("Medicine Tracker");
-		xStage.setResizable(false);
-		Button enter = new Button();
-		enter.setText("Print");
-		enter.setOnAction(new EventHandler<ActionEvent>() {
+		Button print = new Button();
+		print.setText("Print");
+		print.setLayoutX(20);
+		print.setLayoutY(20);
+		print.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent e) {
 				System.out.println("Printing...");
 			}
 		});
+		Button add = new Button();
+		add.setText("Add");
+		add.setLayoutX(440);
+		add.setLayoutY(20);
+		add.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				GUI.addMedPage(xStage);
+			}
+		});
+		Text t = new Text("Home Menu");
+		t.setLayoutX(200);
+		t.setLayoutY(45);
+		t.setFont(new Font(20));
 		Rectangle rec = new Rectangle(0, 0, 500, 80);
 		AnchorPane root = new AnchorPane(rec);
 		rec.setFill(Color.ORANGERED);
-		root.getChildren().add(enter);
+		root.getChildren().add(print);
+		root.getChildren().add(add);
+		root.getChildren().add(t);
 		xStage.setScene(new Scene(root, 500, 350));
 	}
 
@@ -41,9 +59,11 @@ public class GUI {
 				System.out.println("Start!");
 			}
 		});
-		Pane root = new Pane();
+		Rectangle rec = new Rectangle(0, 0, 500, 80);
+		AnchorPane root = new AnchorPane(rec);
+		rec.setFill(Color.ORANGERED);
 		root.getChildren().add(enter);
-		xStage.setScene(new Scene(root, 300, 250));
+		xStage.setScene(new Scene(root, 500, 350));
 		xStage.show();
 	}
 	
@@ -56,9 +76,75 @@ public class GUI {
 				System.out.println("Start!");
 			}
 		});
-		Pane root = new Pane();
+		Rectangle rec = new Rectangle(0, 0, 500, 80);
+		AnchorPane root = new AnchorPane(rec);
+		rec.setFill(Color.ORANGERED);
 		root.getChildren().add(enter);
-		xStage.setScene(new Scene(root, 300, 250));
+		xStage.setScene(new Scene(root, 500, 350));
+		xStage.show();
+	}
+	public static void addMedPage(Stage xStage) {
+		Button enter = new Button();
+		enter.setText("Start");
+		enter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Start!");
+			}
+		});
+		Rectangle rec = new Rectangle(0, 0, 500, 80);
+		AnchorPane root = new AnchorPane(rec);
+		rec.setFill(Color.ORANGERED);
+		root.getChildren().add(enter);
+		xStage.setScene(new Scene(root, 500, 350));
+		xStage.show();
+	}
+	public static void pageFive(Stage xStage) {
+		Button enter = new Button();
+		enter.setText("Start");
+		enter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Start!");
+			}
+		});
+		Rectangle rec = new Rectangle(0, 0, 500, 80);
+		AnchorPane root = new AnchorPane(rec);
+		rec.setFill(Color.ORANGERED);
+		root.getChildren().add(enter);
+		xStage.setScene(new Scene(root, 500, 350));
+		xStage.show();
+	}
+	public static void pageSix(Stage xStage) {
+		Button enter = new Button();
+		enter.setText("Start");
+		enter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Start!");
+			}
+		});
+		Rectangle rec = new Rectangle(0, 0, 500, 80);
+		AnchorPane root = new AnchorPane(rec);
+		rec.setFill(Color.ORANGERED);
+		root.getChildren().add(enter);
+		xStage.setScene(new Scene(root, 500, 350));
+		xStage.show();
+	}
+	public static void pageSeven(Stage xStage) {
+		Button enter = new Button();
+		enter.setText("Start");
+		enter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				System.out.println("Start!");
+			}
+		});
+		Rectangle rec = new Rectangle(0, 0, 500, 80);
+		AnchorPane root = new AnchorPane(rec);
+		rec.setFill(Color.ORANGERED);
+		root.getChildren().add(enter);
+		xStage.setScene(new Scene(root, 500, 350));
 		xStage.show();
 	}
 }
